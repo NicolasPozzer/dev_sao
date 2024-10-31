106a68be93f519500a301b60dcd3ed170d67eede
NicolasPozzer <pozzernico@gmail.com>
2024-10-30 21:12:41 -0300

    Update: nuevos ejercicios hechos

--- src/Extras/c/Main.java
+++ src/Extras/c/Main.java
@@ -1,11 +1,132 @@
 package Extras.c; // Borrar a la hora de subir a omegaUp
 
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+
+        //buena semana = 25% reintegro para cartera general y 40% cart. premium
+        //tiene tope de reintegro de 4mil la general y 6k la premium (por transaccion).
+        //20% reintegro con modo por unica vez una vez alcanzado, no por transaccion.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        n = sc.nextInt();
+
+
+
+        for (int i = 0; i < n; i++) {
+            double precio = 0.00;
+            String tipoDeCartera = "";
+            String buenaOmalaOnda = "";
+            String metodoDePago = "";
+            int cantCuotas = 0;
+
+            //System.out.println("Precio del producto: ");
+            precio = sc.nextDouble();
+
+            //System.out.println("G para cartera General 25% o P para cartera Premium 40%: ");
+            tipoDeCartera = sc.next();
+
+            //System.out.println("B para negocio Buena Onda o M para negocio Mala Onda: ");
+            buenaOmalaOnda = sc.next();
+
+            //System.out.println("T para tarjeta o M para Modo: ");
+            metodoDePago = sc.next();
+
+            //System.out.println("1 o 3 indicando las cuotas seleccionadas: ");
+            cantCuotas = sc.nextInt();
+
+            int contadorTransacciones = 0;
+            double reintegro = 0.00;
+            double calculoTransacciones = 0.00;
+            double agregado = 0.00;
+
+            if (Objects.equals(tipoDeCartera, "G")) {
+
+                //if para reintegros de 4mil
+                if (precio >= 16000.00){
+
+                    boolean stop = false;
+                    double ultimoCalculoGuardado = 0.00;
+                    while (stop != true) {
+                        calculoTransacciones = precio / contadorTransacciones;
+                        if (calculoTransacciones >= 16000.00){
+                            contadorTransacciones++;
+                        }else {
+                            //ultimoCalculoGuardado = calculoTransacciones;
+                            stop = true;
+                        }
+                    }
+                    //ultimoCalculoGuardado = ultimoCalculoGuardado * 0.25;
+
+
+
+                    reintegro = (contadorTransacciones * 4000);
+
+                }else{
+                    reintegro = precio * 0.25;
+                }
+                System.out.println(contadorTransacciones);
+                System.out.printf("%.2f\n",reintegro);
+                double total = 0.00F;
+                total = precio - reintegro;
+                System.out.printf("%.2f\n",total);
+
+            }else {
+
+
+                    //if para reintegros de 4mil
+                    if (precio >= 15000.00){
+
+                        boolean stop = false;
+                        double ultimoCalculoGuardado = 0.00;
+                        double resto = 0.00;
+                        while (stop != true) {
+
+                            calculoTransacciones = precio / contadorTransacciones;
+                            if (calculoTransacciones >= 15000.00){
+                                contadorTransacciones++;
+                            }else {
+                                if(contadorTransacciones == 2){
+                                    contadorTransacciones = 1;
+                                    resto = (precio - 15000) * 0.40F;
+
+                                }
+                                stop = true;
+                            }
+                        }
+
+                        reintegro = (contadorTransacciones * 6000) + resto;
+
+                    }else{
+                        if(buenaOmalaOnda.equals("B") && precio == 10000 ){
+                            reintegro = (precio * 0.40F) + 2000;
+                        } else if (buenaOmalaOnda.equals("M") && precio == 10000 ){
+                            reintegro = (precio * 0.40F) + 2000;
+                        }
+                        else {
+                            reintegro = precio * 0.40F;
+                        }
+                    }
+                    if(contadorTransacciones > 0){
+                        System.out.println(contadorTransacciones);
+                    } else if (precio == 1000) {
+                        contadorTransacciones = 1;
+                        System.out.println(contadorTransacciones);
+                    }
+
+                System.out.printf("%.2f\n",reintegro);
+                    double total = 0.00;
+                    total = precio - reintegro;
+                    System.out.printf("%.2f\n",total);
+
+            }
+        }
 
 
 
--- src/Extras/d/Main.java
+++ src/Extras/d/Main.java
@@ -1,76 +1,30 @@
 package Extras.d;
 
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        // Definimos los topes de reintegro
-        final double TOPE_GENERAL = 3000.0;
-        final double TOPE_PREMIUM = 6000.0;
-        final double TOPE_MODO = 2000.0;
+        Scanner teclado = new Scanner(System.in);
 
-        // Porcentajes de reintegro
-        final double REINTEGRO_GENERAL = 0.25;
-        final double REINTEGRO_PREMIUM = 0.40;
-        final double REINTEGRO_MODO = 0.20;
+        int A = 0;
+        int B = 0;
+        int C = 0;
 
-        // Leemos la cantidad de compras a realizar
-        int N = sc.nextInt();
-        sc.nextLine(); // Para consumir el salto de línea
+        A = teclado.nextInt();
+        B = teclado.nextInt();
+        C = teclado.nextInt();
 
-        for (int i = 0; i < N; i++) {
-            // Leemos los datos de la compra
-            double precio = sc.nextDouble();
-            sc.nextLine();
-            char cartera = sc.nextLine().charAt(0); // G para General o P para Premium
-            char onda = sc.nextLine().charAt(0); // B para Buena Onda o M para Mala Onda
-            char medio = sc.nextLine().charAt(0); // T para Tarjeta o M para Modo
-            int cuotas = sc.nextInt();
-            sc.nextLine(); // Consumimos el salto de línea
+        int contarMultiplo = 0;
 
-            double reintegro = 0;
-            double topeReintegro = 0;
-
-            // Determinamos el tipo de cartera y el reintegro correspondiente
-            if (medio == 'T') {
-                if (cartera == 'G') {
-                    reintegro = precio * REINTEGRO_GENERAL;
-                    topeReintegro = TOPE_GENERAL;
-                } else if (cartera == 'P') {
-                    reintegro = precio * REINTEGRO_PREMIUM;
-                    topeReintegro = TOPE_PREMIUM;
-                }
-            } else if (medio == 'M') {
-                reintegro = precio * REINTEGRO_MODO;
-                topeReintegro = TOPE_MODO;
+        for(int x = A; x <= B; x++){
+            if(x % C == 0){
+                contarMultiplo = contarMultiplo + 1;
             }
-
-            // Aplicamos el tope de reintegro
-            if (reintegro > topeReintegro) {
-                reintegro = topeReintegro;
-            }
-
-            // Calculamos el precio final
-            double precioFinal = precio - reintegro;
-
-            // Si el comercio es buena onda, dividimos en transacciones
-            if (onda == 'B' && medio == 'T') {
-                int transacciones = (int) Math.ceil(precio / topeReintegro);
-                System.out.println(transacciones);
-            } else if (onda == 'B' && medio == 'M') {
-                // En el caso de Modo, se aplica una única vez el reintegro con tope mensual
-                System.out.println(1);
-            }
-
-            // Imprimimos los resultados con 2 decimales de precisión
-            System.out.printf("%.2f\n", reintegro);
-            System.out.printf("%.2f\n", precioFinal);
         }
 
-        sc.close();
-
+        System.out.println(contarMultiplo);
 
     }
 }
