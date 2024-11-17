f71d0c62dd33e043170f8fe0039bcdf1c8a16196
NicolasPozzer <pozzernico@gmail.com>
2024-11-17 15:05:39 -0300

    Update: Integracion de interaccion con el usuario TP SAO

--- src/TP/Main.java
+++ src/TP/Main.java
@@ -12,16 +12,37 @@ public class Main {
         double montoAInvertir = 0.0;
         double mayorRetorno = 0.0;
 
+        System.out.println("\t---INVERSIONES---\n");
+
         //scanner
         Scanner teclado = new Scanner(System.in).useLocale(Locale.US);//locale us para el punto
 
+        System.out.print("Ingrese la cantidad de Dinero que desea invertir: ");
         montoAInvertir = teclado.nextDouble();
+
+        System.out.println("\nIngrese con la cantidad de aplicaciones que desea comparar(max. 3): ");
+        System.out.println("1 = Uala");
+        System.out.println("2 = Uala y Naranja X");
+        System.out.println("3 = Uala, Naranja X y PersonalPay");
         cantAplicacion = teclado.nextInt();
 
         //recorrido y evaluacion de cada App
         for (int i = 1; i <= cantAplicacion; i++) {
             double calcular = 0.0;
+
+            switch (i) {
+                case 1:
+                    System.out.println("Tasa Nominal Anual de Uala (Ej. 0.45 = 45%): ");
+                    break;
+                case 2:
+                    System.out.println("Tasa Nominal Anual de Naranja X: ");
+                    break;
+                case 3:
+                    System.out.println("Tasa Nominal Anual de PersonalPay: ");
+            }
             double tasaNominal = teclado.nextDouble();
+
+            System.out.println("Tope de Inversion: ");
             double topeInversion = teclado.nextDouble();
 
             //llamamos a la funcion y guardamos su resultado para luego comparar su retorno
@@ -33,8 +54,23 @@ public class Main {
             }
         }
 
-        //imprimimos resultado
-        System.out.println(appRecomendada);
+
+        //imprimimos el resultado con switch
+        switch (appRecomendada) {
+            case 1:
+                System.out.println("La Aplicacion Mas recomendada a invertir es la: " + appRecomendada + " Uala con una " +
+                        "tasa de 45% anual!!!");
+                break;
+            case 2:
+                System.out.println("La Aplicacion Mas recomendada a invertir es la: " + appRecomendada + " Naranja X con una " +
+                        "tasa de 42% anual!!!");
+                break;
+            case 3:
+                System.out.println("La Aplicacion Mas recomendada a invertir es la: " + appRecomendada + " Personal Pay con una " +
+                        "tasa de 37% anual sin tope de inversion!!!");
+                break;
+        }
+
     }
 
 
